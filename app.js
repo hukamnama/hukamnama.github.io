4d08e2c18985e5625ecf8f391a731f82ed486d0e
hukamnama <hukamnamagithub@gmail.com>
2017-06-15 21:16:17 +0530

    Create app.js

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-var app = angular.module('plunker', []);
+var pageExecute = {
 
+    fileContents:"Null",
+    pagePrefix:"Null",
+    slides:"Null",
 
-app.controller('MainCtrl', function($scope) {
-  $scope.name = 'World';
-});
+    init: function () {
+        $.ajax({
+            url: "http://shinecoachings.com/~aeon/hukamnama/hukamnama_eng/paragraph.txt",
+            async: false,
+            success: function (data){
+                pageExecute.fileContents = data;
+            }
+        });
+    }
+};
